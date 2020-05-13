4b4a0b9c3cfe306a36eca3c56dce67bb71d3f47a
nalezhytyi <Jediknight05>
2020-05-13 19:53:00 +0300

    Updates

---
+++ precache-manifest.27731a6f81f8022541cfc9b19fd663be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2051eecbac3a26c06031318f7f687b6",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "a5e3befec70a0e8ee4bc",
+    "url": "/covid/static/css/main.5a9f06b0.chunk.css"
+  },
+  {
+    "revision": "07b0582deafec24512b4",
+    "url": "/covid/static/js/2.ac5db47c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.ac5db47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e3befec70a0e8ee4bc",
+    "url": "/covid/static/js/main.44c8fa1d.chunk.js"
+  },
+  {
+    "revision": "2efb2bde5ae38bc0020d",
+    "url": "/covid/static/js/runtime-main.d61a144f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/image.d7265326.png"
+  }
+]);
